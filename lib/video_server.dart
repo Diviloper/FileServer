6582c044d7beea277f5914adbd8b44160774e152
Diviloper <vdiviloper@gmail.com>
2020-03-16 13:49:43 +0100

    Add expiring time to load videos to remove them from cache and refactor video_server.dart get function to improve readability

--- lib/video_server.dart
+++ lib/video_server.dart
@@ -7,21 +7,12 @@ import 'package:home_file_server/utils.dart';
 class VideoServer extends ResourceController {
   static final _rangeHeaderRegExp = RegExp(r'([a-zA-Z]*)=(\d*)-(\d*)');
   static final _loadedVideos = <String, Uint8List>{};
+  static final _loadedVideosTimes = <String, Timer>{};
 
   @Operation.get()
   Future<Response> getVideo({@Bind.header('Range') String rangeHeader}) async {
     final fileName = request.path.remainingPath;
-    Uint8List videoBytes = _loadedVideos[fileName];
-    if (videoBytes == null) {
-      final videoFile = File(request.path.remainingPath);
-      if (!videoFile.existsSync()) {
-        return Response.notFound();
-      } else if (!['mp4', 'webm', 'ogg'].contains(videoFile.extension)) {
-        return Response.badRequest(body: 'File is not a video or it is an unsupported video');
-      }
-      videoBytes = videoFile.readAsBytesSync();
-      _loadedVideos[fileName] = videoBytes;
-    }
+    final videoBytes = _getOrLoadVideo(fileName);
     final videoLength = videoBytes.lengthInBytes;
     if (rangeHeader == null) {
       return Response.ok(
@@ -42,18 +33,7 @@ class VideoServer extends ResourceController {
     } else if (start != null && end == null) {
       end = min(start + 4194304, videoLength);
     }
-    if (unit != 'bytes' ||
-        (start == null && end == null) ||
-        start > videoLength ||
-        end > videoLength) {
-      return Response(
-          416,
-          <String, dynamic>{
-            'Accept-Ranges': 'bytes',
-            'Content-Range': 'bytes */$videoLength',
-          },
-          null);
-    }
+    _checkValues(unit, start, end, videoLength);
     final videoFragment = videoBytes.sublist(start, end);
     return Response(
         206,
@@ -65,5 +45,51 @@ class VideoServer extends ResourceController {
         },
         videoFragment)
       ..contentType = ContentType('video', fileName.substring(fileName.lastIndexOf('.') + 1));
+  }
+
+  void _removeVideoData(String videoName) {
+    _loadedVideos.remove(videoName);
+    _loadedVideosTimes.remove(videoName);
+  }
+
+  void _updateTimer(String fileName) {
+    _loadedVideosTimes[fileName]?.cancel();
+    _loadedVideosTimes[fileName] = Timer(
+      const Duration(minutes: 30),
+      () => _removeVideoData(fileName),
+    );
+  }
+
+  Uint8List _getOrLoadVideo(String fileName) {
+    if (_loadedVideos.containsKey(fileName)) {
+      _updateTimer(fileName);
+      return _loadedVideos[fileName];
+    } else {
+      final videoFile = File(request.path.remainingPath);
+      if (!videoFile.existsSync()) {
+        throw Response.notFound();
+      } else if (!['mp4', 'webm', 'ogg'].contains(videoFile.extension)) {
+        throw Response.badRequest(body: 'File is not a video or it is an unsupported video');
+      }
+      final videoBytes = videoFile.readAsBytesSync();
+      _loadedVideos[fileName] = videoBytes;
+      _updateTimer(fileName);
+      return videoBytes;
+    }
+  }
+
+  void _checkValues(String unit, int start, int end, int videoLength) {
+    if (unit != 'bytes' ||
+        (start == null && end == null) ||
+        start > videoLength ||
+        end > videoLength) {
+      throw Response(
+          416,
+          <String, dynamic>{
+            'Accept-Ranges': 'bytes',
+            'Content-Range': 'bytes */$videoLength',
+          },
+          null);
+    }
   }
 }
